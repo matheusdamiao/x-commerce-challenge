f3ac9fc0fd9251d40c76123255ccf8cfe89f3138
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-18 14:21:38 -0300

    feat: add typeface Inter to global style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,18 @@
 import GlobalStyle from "@/styles/globalStyle";
+import { Inter } from "@next/font/google";
 import { makeServer } from "../server/mirage";
 
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" });
 }
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={inter.className}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
