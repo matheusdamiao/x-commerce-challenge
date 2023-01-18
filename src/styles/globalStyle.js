9c4c489fab998de6900df752137beb1f0169cf33
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-18 13:34:10 -0300

    feat: add a global style with styled-components

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,19 @@
+const { createGlobalStyle } = require("styled-components");
+
+const GlobalStyle = createGlobalStyle`
+
+
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        padding: 0;
+        margin: 0;
+        font-size: 20px;
+    }
+
+
+`;
+
+export default GlobalStyle;
