25cf256ab41800fe928200d094b727869e3a24a6
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-18 13:37:15 -0300

    feat: add global style to all pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import GlobalStyle from "@/styles/globalStyle";
+import { makeServer } from "../server/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
