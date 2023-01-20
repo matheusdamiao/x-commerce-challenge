3cb3a2f3106d6ca21b2c4f15d9803e79f3ecbf13
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-20 11:20:32 -0300

    feat: create Context API with three main local states

---
+++ src/context/context.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const Context = ({ children }) => {
+  const [allProducts, setAllproducts] = useState([]);
+  const [allFavoriteProducts, setAllFavoriteProducts] = useState([]);
+  const [allMostSoldProducts, setAllMostSoldProducts] = useState([]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        Products: [allProducts, setAllproducts],
+        FavoriteProducts: [allFavoriteProducts, setAllFavoriteProducts],
+        MostSoldProducts: [allMostSoldProducts, setAllMostSoldProducts],
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
