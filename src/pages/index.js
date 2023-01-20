10ed06885a09cfdd49d33d5e67ee321459d65af3
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-20 20:24:50 -0300

    feat: add toast component from react-hot-toast to the top of main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import Header from "./../components/Header";
 import AllProducts from "./../components/AllProducts";
 import { WrapperContent } from "@/styles/Home/style";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <div>
+      <Toaster toastOptions={{ position: "bottom-center" }} />
       <Header />
       <WrapperContent>
         <AllProducts />
