436aef38e8ab642dd053f47a6a38b4023a82a617
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-25 15:25:14 -0300

    feat: add a UI state to render conditionally allProducts or favoriteProducts component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import Header from "./../components/Header";
 import AllProducts from "./../components/AllProducts";
+import FavoriteProducts from "./../components/FavoriteProducts";
+
 import { WrapperContent } from "@/styles/Home/style";
 import { Toaster } from "react-hot-toast";
 
 export default function Home() {
+  const [displayAllProducts, setDisplayAllProducts] = useState(true);
+
   return (
     <div>
       <Toaster toastOptions={{ position: "bottom-center" }} />
-      <Header />
+      <Header
+        setDisplayAllProducts={setDisplayAllProducts}
+        displayAllProducts={displayAllProducts}
+      />
       <WrapperContent>
-        <AllProducts />
+        {displayAllProducts ? <AllProducts /> : <FavoriteProducts />}
       </WrapperContent>
       <div></div>
     </div>
