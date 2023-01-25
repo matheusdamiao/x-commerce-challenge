487a6e6e56a8b755207e03fb7b62c8c1046f3449
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-25 18:47:10 -0300

    refactor: move create and delete favorite functions to the new component FavoriteProduct

--- src/components/AllProducts/index.jsx
+++ src/components/AllProducts/index.jsx
@@ -12,13 +12,12 @@ import {
   Table,
   TableData,
 } from "./style";
+import FavoriteProduct from "./../FavoriteProduct/index.jsx";
 import { createFavoriteProduct, getAllProducts } from "@/Utils/api";
 import { AppContext } from "@/context/context";
 import { toast } from "react-hot-toast";
 
 const index = () => {
-  const [isFavorite, setIsFavorite] = useState(false);
-
   const context = useContext(AppContext);
   // Local state and its update function
   const { Products, FavoriteProducts } = context;
@@ -32,32 +31,13 @@ const index = () => {
   // every time data is updated, we also update our local state
   useEffect(() => {
     setAllproducts([data]);
-    console.log(data?.products);
+    // console.log(data?.products);
   }, [data]);
 
   useEffect(() => {
     setAllFavoriteProducts([favorites]);
     console.log(favorites?.favorites);
   }, [favorites]);
-
-  const addFavorite = async (product) => {
-    try {
-      const favoriteProduct = await createFavoriteProduct(product);
-      console.log(favoriteProduct);
-      if (favoriteProduct.favorite) {
-        mutate("/api/favorites", { ...data, favoriteProduct });
-        setIsFavorite(true);
-        toast.success("Produto favoritado!");
-      }
-    } catch (e) {
-      toast.error("Ops, ocorreu algum erro. Tente novamente");
-    }
-  };
-
-  const deleteFavorite = async (product) => {
-    try {
-    } catch (error) {}
-  };
 
   if (error)
     return (
@@ -103,40 +83,31 @@ const index = () => {
           </TableRowHeader>
         </TableHead>
         <TableBody>
-          {data.products.map((product) => {
-            return (
-              <TableRow key={product.id}>
-                <TableData id="identificacao">
-                  <div id="pictureBox">
-                    <img src="/logoPNG.png" alt="" />
-                  </div>
-                  <div id="nameAndCode">
-                    <p>{product.name}</p>
-                    <small>#{product.code}</small>
-                  </div>
-                </TableData>
-                <TableData> $ {product.price}</TableData>
-                <TableData>{product.sales} vendas</TableData>
-                <TableData>{product.stock} und</TableData>
-                <TableData>
-                  {isFavorite ? (
-                    <BsSuitHeartFill
-                      stroke="black"
-                      size="20"
-                      style={{ color: "red" }}
-                      onClick={() => deleteFavorite(product)}
-                    />
-                  ) : (
-                    <BsHeart
-                      size="20"
-                      style={{ cursor: "pointer", stroke: "10" }}
-                      onClick={() => addFavorite(product)}
-                    />
-                  )}{" "}
-                </TableData>
-              </TableRow>
-            );
-          })}
+          {data.products.length != 0 ? (
+            data.products.map((product) => {
+              return (
+                <TableRow key={product.id}>
+                  <TableData id="identificacao">
+                    <div id="pictureBox">
+                      <img src="/logoPNG.png" alt="" />
+                    </div>
+                    <div id="nameAndCode">
+                      <p>{product.name}</p>
+                      <small>#{product.code}</small>
+                    </div>
+                  </TableData>
+                  <TableData> $ {product.price}</TableData>
+                  <TableData>{product.sales} vendas</TableData>
+                  <TableData>{product.stock} und</TableData>
+                  <TableData>
+                    <FavoriteProduct product={product} />
+                  </TableData>
+                </TableRow>
+              );
+            })
+          ) : (
+            <TableData> No products </TableData>
+          )}
         </TableBody>
       </Table>
     </Wrapper>
