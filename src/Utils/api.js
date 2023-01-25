1e63e0f1570ead1e47cec8ce8f2948bde2158a71
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-25 15:22:21 -0300

    feat: add an async function to post a product to favorites database array

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -22,3 +22,25 @@ export const createProduct = async (product) => {
     return data;
   }
 };
+
+export const createFavoriteProduct = async (product) => {
+  let info = {
+    name: product.name,
+    code: product.code,
+    sales: Number(product.sales),
+    price: Number(product.price),
+    stock: Number(product.stock),
+  };
+
+  const res = await fetch("/api/favorites/", {
+    method: "POST",
+    headers: { "Content-type": "application/json;charset=UTF-8" },
+    body: JSON.stringify(info),
+  });
+
+  if (res.status === 201) {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }
+};
