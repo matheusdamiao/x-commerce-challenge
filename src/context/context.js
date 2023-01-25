c936b766a688b4bba5a924822120317c199a1fc5
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-25 15:26:10 -0300

    refactor: add two more global states: allFavoriteProducts and MostSoldProducts

--- src/context/context.js
+++ src/context/context.js
@@ -4,17 +4,15 @@ export const AppContext = createContext();
 
 export const Context = ({ children }) => {
   const [allProducts, setAllproducts] = useState([]);
-  // const [allFavoriteProducts, setAllFavoriteProducts] = useState([]);
-  // const [allMostSoldProducts, setAllMostSoldProducts] = useState([]);
+  const [allFavoriteProducts, setAllFavoriteProducts] = useState([]);
+  const [allMostSoldProducts, setAllMostSoldProducts] = useState([]);
 
   return (
     <AppContext.Provider
       value={{
-        allProducts,
-        setAllproducts,
-        // Products: { allProducts, setAllproducts },
-        // FavoriteProducts: { allFavoriteProducts, setAllFavoriteProducts },
-        // MostSoldProducts: { allMostSoldProducts, setAllMostSoldProducts },
+        Products: { allProducts, setAllproducts },
+        FavoriteProducts: { allFavoriteProducts, setAllFavoriteProducts },
+        MostSoldProducts: { allMostSoldProducts, setAllMostSoldProducts },
       }}
     >
       {children}
