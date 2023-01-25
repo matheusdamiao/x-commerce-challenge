de59927d433e3441eafe61ce3c9b19404309f17c
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-25 15:35:39 -0300

    feat: fetch favorite products and add a function to create a favorite product in mirage js database

--- src/components/AllProducts/index.jsx
+++ src/components/AllProducts/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import { BsHeart, BsSuitHeartFill } from "react-icons/bs";
 import {
   Wrapper,
@@ -12,23 +12,52 @@ import {
   Table,
   TableData,
 } from "./style";
-import { getAllProducts } from "@/Utils/api";
+import { createFavoriteProduct, getAllProducts } from "@/Utils/api";
 import { AppContext } from "@/context/context";
+import { toast } from "react-hot-toast";
 
 const index = () => {
   const [isFavorite, setIsFavorite] = useState(false);
 
   const context = useContext(AppContext);
   // Local state and its update function
-  const { allProducts, setAllproducts } = context;
+  const { Products, FavoriteProducts } = context;
+  const { allProducts, setAllproducts } = Products;
+  const { allFavoriteProducts, setAllFavoriteProducts } = FavoriteProducts;
 
   const { data, error, isLoading } = useSWR("/api/products", getAllProducts);
+
+  const { data: favorites } = useSWR("/api/favorites", getAllProducts);
 
   // every time data is updated, we also update our local state
   useEffect(() => {
     setAllproducts([data]);
-    console.log(data);
+    console.log(data?.products);
   }, [data]);
+
+  useEffect(() => {
+    setAllFavoriteProducts([favorites]);
+    console.log(favorites?.favorites);
+  }, [favorites]);
+
+  const addFavorite = async (product) => {
+    try {
+      const favoriteProduct = await createFavoriteProduct(product);
+      console.log(favoriteProduct);
+      if (favoriteProduct.favorite) {
+        mutate("/api/favorites", { ...data, favoriteProduct });
+        setIsFavorite(true);
+        toast.success("Produto favoritado!");
+      }
+    } catch (e) {
+      toast.error("Ops, ocorreu algum erro. Tente novamente");
+    }
+  };
+
+  const deleteFavorite = async (product) => {
+    try {
+    } catch (error) {}
+  };
 
   if (error)
     return (
@@ -90,20 +119,20 @@ const index = () => {
                 <TableData>{product.sales} vendas</TableData>
                 <TableData>{product.stock} und</TableData>
                 <TableData>
-                  {!isFavorite && (
-                    <BsHeart
-                      size="20"
-                      style={{ cursor: "pointer", stroke: "10" }}
-                      onClick={() => setIsFavorite(!isFavorite)}
-                    />
-                  )}
-                  {isFavorite && (
+                  {isFavorite ? (
                     <BsSuitHeartFill
                       stroke="black"
                       size="20"
                       style={{ color: "red" }}
+                      onClick={() => deleteFavorite(product)}
                     />
-                  )}
+                  ) : (
+                    <BsHeart
+                      size="20"
+                      style={{ cursor: "pointer", stroke: "10" }}
+                      onClick={() => addFavorite(product)}
+                    />
+                  )}{" "}
                 </TableData>
               </TableRow>
             );
