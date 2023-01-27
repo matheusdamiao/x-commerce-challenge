d5e74fa767fe5fed1824346d2534458f04145036
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-27 12:22:18 -0300

    feat: add MostSoldProducts components to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Header from "./../components/Header";
 import AllProducts from "./../components/AllProducts";
 import FavoriteProducts from "./../components/FavoriteProducts";
+import MostSoldProducts from "./../components/MostSoldProducts";
 
 import { WrapperContent } from "@/styles/Home/style";
 import { Toaster } from "react-hot-toast";
@@ -17,9 +18,10 @@ export default function Home() {
         displayAllProducts={displayAllProducts}
       />
       <WrapperContent>
+        <MostSoldProducts />
+
         {displayAllProducts ? <AllProducts /> : <FavoriteProducts />}
       </WrapperContent>
-      <div></div>
     </div>
   );
 }
