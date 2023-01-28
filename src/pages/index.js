aed777c24d8cd6c3745fc7ec96cb86402644691d
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-27 23:25:23 -0300

    feat: add state logic to render searchresult component whenenver search input has some value

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,32 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Header from "./../components/Header";
 import AllProducts from "./../components/AllProducts";
 import FavoriteProducts from "./../components/FavoriteProducts";
 import MostSoldProducts from "./../components/MostSoldProducts";
+import SearchResult from "./../components/SearchResult";
 
 import { WrapperContent } from "@/styles/Home/style";
 import { Toaster } from "react-hot-toast";
+import { AppContext } from "@/context/context";
 
 export default function Home() {
   const [displayAllProducts, setDisplayAllProducts] = useState(true);
+  const [hasSearchInput, setHasSearchInput] = useState(false);
+  const context = useContext(AppContext);
+  const { Search } = context;
+  const { searchValue } = Search;
+
+  useEffect(() => {
+    checkSearchValue(searchValue);
+  }, [searchValue]);
+
+  const checkSearchValue = async () => {
+    if (searchValue === "") {
+      setHasSearchInput(false);
+    } else {
+      setHasSearchInput(true);
+    }
+  };
 
   return (
     <div>
@@ -19,8 +37,15 @@ export default function Home() {
       />
       <WrapperContent>
         <MostSoldProducts />
-
-        {displayAllProducts ? <AllProducts /> : <FavoriteProducts />}
+        {!hasSearchInput ? (
+          displayAllProducts ? (
+            <AllProducts />
+          ) : (
+            <FavoriteProducts />
+          )
+        ) : (
+          <SearchResult />
+        )}
       </WrapperContent>
     </div>
   );
