f403e7969b62ea42efbd0a1518dc07f96e038b36
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-27 23:23:52 -0300

    feat: add new route to search products by name

--- src/server/mirage.js
+++ src/server/mirage.js
@@ -84,16 +84,52 @@ export function makeServer({ environment = "test" } = {}) {
         return schema.products.all();
       });
 
+      // this route is being used to filter products by name
+      this.get("/products/name", (schema, request) => {
+        let page = parseInt(request.queryParams.page);
+        let limit = parseInt(request.queryParams.limit);
+        let searchValue = request.queryParams.search;
+        let startIndex = (page - 1) * limit;
+        let endIndex = page * limit;
+        let allProducts = schema.db.products;
+
+        //check if there's some product left from the division into pages.
+        // if there is, add one more page to the total pages,
+        // otherwise set total pages to be the division between all products and the page's limit
+        let productsLeft = allProducts.length % limit;
+        let totalPages;
+        if (!productsLeft) {
+          totalPages = Math.floor(allProducts.length / limit);
+        } else {
+          totalPages = Math.floor(allProducts.length / limit) + 1;
+        }
+
+        // filter only the products whose name property matches searchValue
+        let filtered = allProducts.filter((product) =>
+          product.name.toLowerCase().includes(searchValue)
+        );
+
+        let result = filtered.slice(startIndex, endIndex);
+
+        let data = {
+          result,
+          totalPages,
+        };
+        return data;
+      });
+
+      // this route is being used to sort all products in descending order according to their sales values.
       this.get(`/productsPaginated`, (schema, request) => {
         let page = parseInt(request.queryParams.page);
         let limit = parseInt(request.queryParams.limit);
         let startIndex = (page - 1) * limit;
         let endIndex = page * limit;
         let allProducts = schema.db.products;
+        // sort all products in a descending order according to their sales values.
         let sortedProducts = allProducts.sort((a, b) => b.sales - a.sales);
         let result = sortedProducts.slice(startIndex, endIndex);
 
-        //check if there's some product left.
+        //check if there's some product left from the division into pages.
         // if there is, add one more page to the total pages,
         // otherwise set total pages to be the division between all products and the page's limit
         let productsLeft = allProducts.length % limit;
@@ -121,6 +157,7 @@ export function makeServer({ environment = "test" } = {}) {
         return schema.favorites.all();
       });
 
+      //this route is being used to create a favorite product based on the product's values
       this.post("/favorites", (schema, request) => {
         let attrs = JSON.parse(request.requestBody);
         return schema.favorites.create(attrs);
